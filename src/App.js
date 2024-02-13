b09242ff82ed45c57917e8acdd4be8b72c430a6b
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-02-13 13:35:08 -0500

    modal 1 functioning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
+import React, { useState } from 'react';
+
+import ModalManager from './components/Modals/ModalManager';
 import PreLoader from './components/Preloader/PreLoader';
 
+import './App.css';
+import MainPage from './components/Main/MainPage';
+
 function App() {
+  const[modalOpen, setModal] = useState(false);
+
+  const openModal = event => {
+    const { target: { dataset: { modal }}} = event
+    if (modal) setModal(modal)
+  };
+
+  const closeModal = () => {
+    setModal('')
+  }
+
   return (
    <>
     <PreLoader />
+    <div className='app--shell' onClick={openModal}>
+      <MainPage />
+      <ModalManager closeFn={closeModal} modal={modalOpen}/>
+    </div>
 
-    NANIMAL.DEV
    </>
 
   );
