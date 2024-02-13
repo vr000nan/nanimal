38f387c6ba9f6a75e419b5b5105e6e504837cc34
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-02-13 16:20:55 -0500

    buttons working, need to fix samples and history

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,24 +1,30 @@
 import React from "react";
 // import { Route, Switch } from 'react-router-dom';
+import './mainPageStyles.css';
 
 const MainPage = ({}) => {
 
     return(
         <div style={{ display: 'flex', columnGap: '1rem' }}>
-        <button type="button" data-modal="modal-samples">
-          Samples
+        <button type="button" data-modal="modal-samples" class="cybr-btn">
+          samples
+          <span aria-hidden class="cybr-btn__glitch">sǝʅdɯɐs</span>
         </button>
-        <button type="button" data-modal="modal-history">
-          History
+        <button type="button" data-modal="modal-history" class="cybr-btn">
+          history
+          <span aria-hidden class="cybr-btn__glitch">ʎɹoʇsᴉɥ</span>
         </button>
-        <button type="button" data-modal="modal-about">
-          About
+        <button type="button" data-modal="modal-about" class="cybr-btn">
+          about
+          <span aria-hidden class="cybr-btn__glitch">ʇnoqɐ</span>
         </button>
-        <button type="button" data-modal="modal-skills">
-          Skills
+        <button type="button" data-modal="modal-skills" class="cybr-btn">
+          skills
+          <span aria-hidden class="cybr-btn__glitch">sʅʅᴉʞs</span>
         </button>
-        <button type="button" data-modal="modal-contact">
-          Contact
+        <button type="button" data-modal="modal-contact" class="cybr-btn">
+          contact
+          <span aria-hidden class="cybr-btn__glitch">ʇɔɐʇuoɔ</span>
         </button>
         </div>
     )
