e0e79d634ecfa6cc163463748e3ac5a061dd76b3
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-02-13 21:26:58 -0500

    buttons aligned, history still needs fixing, and all need to be placed aournd Placeholder image

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #PreLoader
     Animation created by Sharmil001
     https://github.com/Sharmil001/myportfolio
+
+#Cyber Buttons
+    Styling and animation created by Jhey
+    https://codepen.io/jh3y/pen/PoGbxLp
--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,31 +1,31 @@
 import React from "react";
-// import { Route, Switch } from 'react-router-dom';
 import './mainPageStyles.css';
 
 const MainPage = ({}) => {
 
     return(
-        <div style={{ display: 'flex', columnGap: '1rem' }}>
-        <button type="button" data-modal="modal-samples" class="cybr-btn">
-          samples
-          <span aria-hidden class="cybr-btn__glitch">sǝʅdɯɐs</span>
-        </button>
-        <button type="button" data-modal="modal-history" class="cybr-btn">
-          history
-          <span aria-hidden class="cybr-btn__glitch">ʎɹoʇsᴉɥ</span>
-        </button>
-        <button type="button" data-modal="modal-about" class="cybr-btn">
-          about
-          <span aria-hidden class="cybr-btn__glitch">ʇnoqɐ</span>
-        </button>
-        <button type="button" data-modal="modal-skills" class="cybr-btn">
-          skills
-          <span aria-hidden class="cybr-btn__glitch">sʅʅᴉʞs</span>
-        </button>
-        <button type="button" data-modal="modal-contact" class="cybr-btn">
-          contact
-          <span aria-hidden class="cybr-btn__glitch">ʇɔɐʇuoɔ</span>
-        </button>
+        <div style={{ display: 'block', columnGap: '0.3rem' }}>
+          <button type="button" data-modal="modal-samples" class="cybr-btn">
+            samples
+            <span aria-hidden class="cybr-btn__glitch">sǝʅdɯɐs</span>
+          </button>
+          <button type="button" data-modal="modal-history" class="cybr-btn">
+            history
+            <span aria-hidden class="cybr-btn__glitch">ʎɹoʇsᴉɥ</span>
+          </button>
+          <img src="/Placeholder.png" alt="mainImage" />
+          <button type="button" data-modal="modal-about" class="cybr-btn">
+            about
+            <span aria-hidden class="cybr-btn__glitch">ʇnoqɐ</span>
+          </button>
+          <button type="button" data-modal="modal-skills" class="cybr-btn">
+            skills
+            <span aria-hidden class="cybr-btn__glitch">sʅʅᴉʞs</span>
+          </button>
+          <button type="button" data-modal="modal-contact" class="cybr-btn">
+            contact
+            <span aria-hidden class="cybr-btn__glitch">ʇɔɐʇuoɔ</span>
+          </button>
         </div>
     )
 }
