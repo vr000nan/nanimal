b16c1fbef33820b8c88b004eadf385d04211f966
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-02-18 12:04:04 -0500

    setting up default modal styles

--- src/components/Modals/About.js
+++ src/components/Modals/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Modal from "./Modal";
 import './ModalStyles/modalStyles.css'
+import './ModalStyles/aboutStyles.css';
 
 const About = ({ closeFn = () => null, open = false }) => {
     return (
@@ -8,32 +9,22 @@ const About = ({ closeFn = () => null, open = false }) => {
         <div className="modal--mask">
           <div className="modal-window">
             <header className="modal--header">
-              
-              <div className="modal--x">
-                <button type="button" onClick={closeFn}>
+                <button className="modal--x" type="button" onClick={closeFn}>
                   X
                 </button>
-               </div>
-               <h1>About</h1>
-              </header>
+              <h1>About</h1>
+            </header>
             <div className="modal--body">
+            <img src="/PlaceholderAbout.png" alt="aboutImage" className="about-image" />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Viverra aliquet eget sit amet tellus cras. Sit amet est placerat in egestas erat imperdiet. 
                 Lacus vestibulum sed arcu non odio. Augue neque gravida in fermentum et. 
-                Ornare aenean euismod elementum nisi quis eleifend quam. Consequat interdum varius sit amet. 
-                Non diam phasellus vestibulum lorem sed risus ultricies tristique. Nibh tortor id aliquet lectus proin nibh. 
-                Arcu non odio euismod lacinia at quis risus. Nec dui nunc mattis enim ut. Purus in mollis nunc sed. Dui vivamus arcu felis bibendum ut. 
-                Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus.
 
                 <br></br>
                 
                 Lacinia quis vel eros donec ac odio. Ac ut consequat semper viverra nam libero justo laoreet. P
                 orta lorem mollis aliquam ut. Ornare arcu dui vivamus arcu felis. Vel eros donec ac odio tempor orci dapibus ultrices. 
-                Adipiscing vitae proin sagittis nisl rhoncus. Tortor id aliquet lectus proin nibh. Maecenas ultricies mi eget mauris. 
-                Aliquet nibh praesent tristique magna sit amet. Venenatis tellus in metus vulputate eu scelerisque. 
-                Eget arcu dictum varius duis at consectetur. Dolor sit amet consectetur adipiscing elit duis. 
-                Morbi blandit cursus risus at ultrices mi tempus imperdiet nulla. Eu lobortis elementum nibh tellus molestie nunc. 
-                Dolor sit amet consectetur adipiscing elit.</p>
+                Adipiscing vitae proin sagittis nisl rhoncus. Tortor id aliquet lectus proin nibh. </p>
             </div>
           </div>
         </div>
