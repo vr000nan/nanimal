0c2935e38fd03a4cf105df365fe147b1be823b8e
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-02-26 12:09:57 -0500

    basic styling added to contact modal

--- src/components/Modals/Contact.js
+++ src/components/Modals/Contact.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import Modal from "./Modal";
 import './ModalStyles/modalStyles.css'
+import './ModalStyles/contactStyles.css'
 import emailjs from '@emailjs/browser';
 
 const Contact = ({ closeFn = () => null, open = false }) => {
@@ -46,7 +47,7 @@ const Contact = ({ closeFn = () => null, open = false }) => {
         <div className="modal-window">
 
           <div className="modal--body">
-            <h1>Contact
+            <h1>Contact Me
               <button type="button" onClick={closeFn}>
                 X
               </button>
