ee3a1bc28a0bcfcaf5a442a8ece3d0f5ebf672dd
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-02-27 14:26:22 -0500

    resume updated

--- src/components/Resume/PDFFile.js
+++ src/components/Resume/PDFFile.js
@@ -1,401 +1,284 @@
+import React from 'react';
 import {
-    Link,
-    Text,
-    Font,
-    Page,
-    View,
-    Image,
-    Document,
-    StyleSheet,
-  } from "@react-pdf/renderer";
-  
-  const headerStyles = StyleSheet.create({
-    container: {
-      flexDirection: "row",
-      borderBottomWidth: 2,
-      borderBottomColor: "#112131",
-      borderBottomStyle: "solid",
-      alignItems: "stretch",
-    },
-    detailColumn: {
-      flexDirection: "column",
-      flexGrow: 9,
-      textTransform: "uppercase",
-    },
-    linkColumn: {
-      flexDirection: "column",
-      flexGrow: 2,
-      alignSelf: "flex-end",
-      justifySelf: "flex-end",
-    },
-    name: {
-      fontSize: 24,
-      fontFamily: "Lato Bold",
-    },
-    subtitle: {
-      fontSize: 10,
-      justifySelf: "flex-end",
-      fontFamily: "Lato",
-    },
-    link: {
-      fontFamily: "Lato",
-      fontSize: 10,
-      color: "black",
-      textDecoration: "none",
-      alignSelf: "flex-end",
-      justifySelf: "flex-end",
-    },
-  });
-  
-  const Header = () => (
-    <View style={headerStyles.container}>
-      <View style={headerStyles.detailColumn}>
-        <Text style={headerStyles.name}>Luke Skywalker</Text>
-        <Text style={headerStyles.subtitle}>Jedi Master</Text>
-      </View>
-      <View style={headerStyles.linkColumn}>
-        <Link href="mailto:luke@theforce.com" style={headerStyles.link}>
-          <Text>luke@theforce.com</Text>
-        </Link>
-      </View>
-    </View>
-  );
-  
-  const titleStyles = StyleSheet.create({
-    title: {
-      fontFamily: "Lato Bold",
-      fontSize: 14,
-      marginBottom: 10,
-      textTransform: "uppercase",
-    },
-  });
-  
-  const Title = ({ children }) => (
-    <Text style={titleStyles.title}>{children}</Text>
-  );
-  
-  const listStyles = StyleSheet.create({
-    item: {
-      flexDirection: "row",
-      marginBottom: 5,
-    },
-    bulletPoint: {
-      width: 10,
-      fontSize: 10,
-    },
-    itemContent: {
-      flex: 1,
-      fontSize: 10,
-      fontFamily: "Lato",
-    },
-  });
-  
-  const List = ({ children }) => children;
-  
-  const Item = ({ children }) => (
-    <View style={listStyles.item}>
-      <Text style={listStyles.bulletPoint}>•</Text>
-      <Text style={listStyles.itemContent}>{children}</Text>
-    </View>
-  );
-  
-  const skilsStyles = StyleSheet.create({
-    title: {
-      fontFamily: "Lato Bold",
-      fontSize: 11,
-      marginBottom: 10,
-    },
-    skills: {
-      fontFamily: "Lato",
-      fontSize: 10,
-      marginBottom: 10,
-    },
-  });
-  
-  const SkillEntry = ({ name, skills }) => (
-    <View>
-      <Text style={skilsStyles.title}>{name}</Text>
-      <List>
-        {skills.map((skill, i) => (
-          <Item key={i}>{skill}</Item>
-        ))}
-      </List>
-    </View>
-  );
-  
-  const Skills = () => (
-    <View>
-      <Title>Skills</Title>
-      <SkillEntry
-        name="Combat Abilities"
-        skills={[
-          "Completed Jedi Master training and built a lightsaber from scratch in order to do battle against the Empire",
-          "Defeated the Rancor and rescued Princess Leia from Jabba the Hutt",
-          "Competent fighter pilot as well as an excelent shot with nearly any weapon",
-        ]}
-      />
-    </View>
-  );
-  
-  const educationStyles = StyleSheet.create({
-    container: {
-      marginBottom: 10,
-    },
-    school: {
-      fontFamily: "Lato Bold",
-      fontSize: 10,
-    },
-    degree: {
-      fontFamily: "Lato",
-      fontSize: 10,
-    },
-    candidate: {
-      fontFamily: "Lato Italic",
-      fontSize: 10,
-    },
-  });
-  
-  const Education = () => (
-    <View style={educationStyles.container}>
-      <Title>Education</Title>
-      <Text style={educationStyles.school}>Jedi Academy</Text>
-      <Text style={educationStyles.degree}>Jedi Master</Text>
-      <Text style={educationStyles.candidate}>A long, long time ago</Text>
-    </View>
-  );
-  
-  const expStyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 30,
-      paddingLeft: 15,
-      "@media max-width: 400": {
-        paddingTop: 10,
-        paddingLeft: 0,
-      },
-    },
-    entryContainer: {
-      marginBottom: 10,
-    },
-    date: {
-      fontSize: 11,
-      fontFamily: "Lato Italic",
-    },
-    detailContainer: {
-      flexDirection: "row",
-    },
-    detailLeftColumn: {
-      flexDirection: "column",
-      marginLeft: 10,
-      marginRight: 10,
-    },
-    detailRightColumn: {
-      flexDirection: "column",
-      flexGrow: 9,
-    },
-    bulletPoint: {
-      fontSize: 10,
-    },
-    details: {
-      fontSize: 10,
-      fontFamily: "Lato",
-    },
-    headerContainer: {
-      flexDirection: "row",
-      marginBottom: 10,
-    },
-    leftColumn: {
-      flexDirection: "column",
-      flexGrow: 9,
-    },
-    rightColumn: {
-      flexDirection: "column",
-      flexGrow: 1,
-      alignItems: "flex-end",
-      justifySelf: "flex-end",
-    },
-    title: {
-      fontSize: 11,
-      color: "black",
-      textDecoration: "none",
-      fontFamily: "Lato Bold",
-    },
-  });
-  
-  const ExperienceEntry = ({ company, details, position, date }) => {
-    const title = `${company} | ${position}`;
-    return (
-      <View style={expStyles.entryContainer}>
-        <View style={expStyles.headerContainer}>
-          <View style={expStyles.leftColumn}>
-            <Text style={expStyles.title}>{title}</Text>
-          </View>
-          <View style={expStyles.rightColumn}>
-            <Text style={expStyles.date}>{date}</Text>
-          </View>
-        </View>
-        <List>
-          {details.map((detail, i) => (
-            <Item key={i} style={expStyles.detailContainer}>
-              {detail}
-            </Item>
-          ))}
-        </List>
-      </View>
-    );
-  };
-  
-  const experienceData = [
-    {
-      company: "Jedi Temple, Coruseant",
-      date: "A long time ago...",
-      details: [
-        "Started a new Jedi Temple in order to train the next generation of Jedi Masters",
-        "Discovered and trained a new generation of Jedi Knights, which he recruited from within the New Republic",
-        "Communicates with decesased Jedi Masters such as Anakin Skywalker, Yoda, Obi-Wan Kenobi in order to learn the secrets of the Jedi Order",
-      ],
-      position: "Head Jedi Master",
-    },
-    {
-      company: "Rebel Alliance",
-      date: "A long time ago...",
-      details: [
-        "Lead legions of troops into battle while demonstrating bravery, competence and honor",
-        "Created complicated battle plans in conjunction with other Rebel leaders in order to ensure the greatest chance of success",
-        "Defeated Darth Vader in single-combat, and convinced him to betray his mentor, the Emperor",
-      ],
-      position: "General",
-    },
-    {
-      company: "Rebel Alliance",
-      date: "A long time ago...",
-      details: [
-        "Destroyed the Death Star by using the force to find its only weakness and delivering a torpedo into the center of the ship",
-        "Commanded of squadron of X-Wings into battle",
-        "Defeated an enemy AT-AT single handedly after his ship was destroyed",
-        "Awarded a medal for valor and bravery in battle for his successful destruction of the Death Star",
-      ],
-      position: "Lieutenant Commander",
-    },
-    {
-      company: "Tatooine Moisture Refinery",
-      date: "A long time ago...",
-      details: [
-        "Replaced damaged power converters",
-        "Performed menial labor thoughout the farm in order to ensure its continued operation",
-      ],
-      position: "Moisture Farmer",
-    },
-  ];
-  
-  const Experience = () => (
-    <View style={expStyles.container}>
-      <Title>Experience</Title>
-      {experienceData.map(({ company, date, details, position }) => (
-        <ExperienceEntry
-          company={company}
-          date={date}
-          details={details}
-          key={company + position}
-          position={position}
-        />
-      ))}
-    </View>
-  );
-  
-  const resumeStyles = StyleSheet.create({
-    page: {
-      padding: 30,
-    },
-    container: {
-      flex: 1,
-      flexDirection: "row",
-      "@media max-width: 400": {
-        flexDirection: "column",
-      },
-    },
-    image: {
-      marginBottom: 10,
-    },
-    leftColumn: {
-      flexDirection: "column",
-      width: 170,
-      paddingTop: 30,
-      paddingRight: 15,
-      "@media max-width: 400": {
-        width: "100%",
-        paddingRight: 0,
-      },
-      "@media orientation: landscape": {
-        width: 200,
-      },
-    },
-    footer: {
-      fontSize: 12,
-      fontFamily: "Lato Bold",
-      textAlign: "center",
-      marginTop: 15,
-      paddingTop: 5,
-      borderWidth: 3,
-      borderColor: "gray",
-      borderStyle: "dashed",
-      "@media orientation: landscape": {
-        marginTop: 10,
-      },
-    },
-  });
-  
-  Font.register({
-    family: "Open Sans",
-    src: "https://fonts.gstatic.com/s/opensans/v17/mem8YaGs126MiZpBA-UFVZ0e.ttf",
-  });
-  
-  Font.register({
-    family: "Lato",
-    src: "https://fonts.gstatic.com/s/lato/v16/S6uyw4BMUTPHjx4wWw.ttf",
-  });
-  
-  Font.register({
-    family: "Lato Italic",
-    src: "https://fonts.gstatic.com/s/lato/v16/S6u8w4BMUTPHjxsAXC-v.ttf",
-  });
-  
-  Font.register({
-    family: "Lato Bold",
-    src: "https://fonts.gstatic.com/s/lato/v16/S6u9w4BMUTPHh6UVSwiPHA.ttf",
-  });
-  
-  const Resume = (props) => (
-    <Page {...props} style={resumeStyles.page}>
-      <Header />
-      <View style={resumeStyles.container}>
-        <View style={resumeStyles.leftColumn}>
-          <Image
-            src="https://react-pdf.org/images/luke.jpg"
-            style={resumeStyles.image}
-          />
-          <Education />
-          <Skills />
-        </View>
-        <Experience />
-      </View>
-      <Text style={resumeStyles.footer}>
-        This IS the candidate you are looking for
+  Link,
+  Text,
+  Font,
+  Page,
+  View,
+  Document,
+  StyleSheet,
+} from "@react-pdf/renderer";
+
+const headerStyles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderBottomWidth: 2,
+    borderBottomColor: "#112131",
+    borderBottomStyle: "solid",
+    alignItems: "stretch",
+    marginBottom: 10, // Add margin bottom to create space between sections
+  },
+  detailColumn: {
+    flexDirection: "column",
+    flexGrow: 9,
+    textTransform: "uppercase",
+  },
+  linkColumn: {
+    flexDirection: "column",
+    flexGrow: 2,
+    alignSelf: "flex-end",
+    justifySelf: "flex-end",
+  },
+  name: {
+    fontSize: 24,
+    fontFamily: "Lato Bold",
+  },
+  subtitle: {
+    fontSize: 10,
+    justifySelf: "flex-end",
+    fontFamily: "Lato",
+  },
+  link: {
+    fontFamily: "Lato",
+    fontSize: 10,
+    color: "black",
+    textDecoration: "none",
+    alignSelf: "flex-end",
+    justifySelf: "flex-end",
+  },
+});
+
+const Header = () => (
+  <View style={headerStyles.container}>
+    <View style={headerStyles.detailColumn}>
+      <Text style={headerStyles.name}>Nan Wroblewski</Text>
+      <Text style={headerStyles.subtitle}>Philadelphia, Pennsylvania (Able to Relocate)</Text>
+      <Text style={headerStyles.subtitle}>+1 330-704-0488 | vr000nan@gmail.com</Text>
+      <Text style={headerStyles.link}>
+        <Link href="https://www.linkedin.com/in/nan-wroblewski-09547a224/">LinkedIn</Link> |{' '}
+        <Link href="https://github.com/vr000nan">GitHub</Link> |{' '}
+        <Link href="https://vr000nan.netlify.com">Portfolio</Link>
       </Text>
-    </Page>
-  );
-  
+    </View>
+  </View>
+);
+
+const titleStyles = StyleSheet.create({
+  title: {
+    fontFamily: "Lato Bold",
+    fontSize: 14,
+    marginBottom: 10,
+    textTransform: "uppercase",
+  },
+});
+
+const Title = ({ children }) => (
+  <Text style={titleStyles.title}>{children}</Text>
+);
+
+const listStyles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  bulletPoint: {
+    width: 10,
+    fontSize: 10,
+  },
+  itemContent: {
+    flex: 1,
+    fontSize: 10,
+    fontFamily: "Lato",
+  },
+});
+
+const List = ({ children }) => children;
+
+const Item = ({ children }) => (
+  <View style={listStyles.item}>
+    <Text style={listStyles.bulletPoint}>•</Text>
+    <Text style={listStyles.itemContent}>{children}</Text>
+  </View>
+);
+
+const skillsStyles = StyleSheet.create({
+  title: {
+    fontFamily: "Lato Bold",
+    fontSize: 11,
+    marginBottom: 10,
+  },
+  skills: {
+    fontFamily: "Lato",
+    fontSize: 10,
+    marginBottom: 10,
+  },
+});
+
+const SkillEntry = ({ name, skills }) => (
+  <View>
+    <Text style={skillsStyles.title}>{name}</Text>
+    <List>
+      {skills.map((skill, i) => (
+        <Item key={i}>{skill}</Item>
+      ))}
+    </List>
+  </View>
+);
+
+const Skills = () => (
+  <View>
+    <Title>Skills</Title>
+    <SkillEntry
+      skills={[
+        "My ability to communicate effectively and work seamlessly within cross-functional teams underscores my commitment to fostering a collaborative and inclusive work environment. Eager to contribute my skills and enthusiasm to innovative projects, I am poised to make a positive impact in the realm of web-based software engineering."
+      ]}
+    />
+  </View>
+);
+
+const experienceStyles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  entryContainer: {
+    marginBottom: 10,
+  },
+  title: {
+    fontFamily: "Lato Bold",
+    fontSize: 11,
+    color: "black",
+    textDecoration: "none",
+  },
+  date: {
+    fontSize: 10,
+    fontFamily: "Lato Italic",
+    marginBottom: 5,
+  },
+  details: {
+    fontSize: 10,
+    fontFamily: "Lato",
+  },
+});
+
+const ExperienceEntry = ({ company, position, date, details }) => (
+  <View style={experienceStyles.entryContainer}>
+    <Text style={experienceStyles.title}>{company} | {position}</Text>
+    <Text style={experienceStyles.date}>{date}</Text>
+    <List>
+      {details.map((detail, i) => (
+        <Item key={i}>{detail}</Item>
+      ))}
+    </List>
+  </View>
+);
+
+const Experience = () => (
+  <View style={experienceStyles.container}>
+    <Title>Professional Experience</Title>
+    <ExperienceEntry
+      company="UI/UX Developer"
+      position="Rise Marketing"
+      date="August 2023 - Present"
+      details={[
+        "Proficient in leveraging JavaScript and PHP to create dynamic and responsive user interfaces.",
+        "Developed interactive web applications with a focus on optimizing user experiences through sleek and intuitive designs.",
+        "Skilled in using popular JavaScript frameworks like React to build modern, single-page applications (SPAs). Employed React's component-based architecture to create reusable UI elements, streamlining development workflows.",
+        "Prioritized user-centered design principles, conducting usability testing, and gathering user feedback to inform interface enhancements. Ensured that web applications were visually appealing, user-friendly, and accessible across devices.",
+        "Created responsive web designs using HTML/CSS and JavaScript, ensuring seamless user experiences on various screen sizes and devices.",
+        "Conducted thorough testing and debugging of JavaScript code to identify and resolve issues, ensuring the reliability and stability of web applications.",
+      ]}
+    />
+    <ExperienceEntry
+      company="Software Engineer 1"
+      position="JP Morgan Chase & Co."
+      date="October 2022 - July 2023"
+      details={[
+        "Worked with software development and testing team members to design and develop robust solutions to meet client requirements for functionality, scalability, and performance.",
+        "Led cross-functional teams, implementing cutting-edge software solutions for enhanced operational efficiency.",
+        "Committed to continuous learning and innovation, contributing breakthrough ideas to drive excellence.",
+        "Ensured top-notch code quality through rigorous reviews, delivering high-performing systems.",
+        "Modernized legacy code, elevating functionality, and stability.",
+        "Recognized for transforming complex systems into streamlined platforms.",
+        "Wrote and tested front end components with React, react-testing library, and MDS styling.",
+        "Gained knowledge on Java, SpringBoot, and Typescript and utilized it on a daily basis.",
+      ]}
+    />
+    <ExperienceEntry
+      company="Mentor/Instructor Associate"
+      position="Fullstack Academy"
+      date="January 2022 - Present"
+      details={[
+        "Exemplified exceptional quality control, meticulously evaluating student submissions against grading rubrics to ensure compliance with requirements.",
+        "Served as a dedicated mentor and invaluable support to coding bootcamp students, fostering their success throughout the program and facilitating their smooth transition into the tech industry.",
+        "Proactively collaborated with instructors to enhance the curriculum and instructional materials, resulting in a superior learning experience and outcomes for students.",
+        "Demonstrated keen analytical skills by conducting regular assessments of student progress, tracking their learning journeys, and providing targeted feedback for continuous improvement.",
+      ]}
+    />
+  </View>
+);
+
+const educationStyles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  title: {
+    fontFamily: "Lato Bold",
+    fontSize: 11,
+    marginBottom: 10,
+  },
+  details: {
+    fontFamily: "Lato",
+    fontSize: 10,
+  },
+});
+
+const Education = () => (
+  <View style={educationStyles.container}>
+    <Title>Education</Title>
+    <Text style={educationStyles.title}>Certificate in Web Development and Software Engineering, December 2021</Text>
+    <Text style={educationStyles.details}>University Of North Florida Remote, Florida</Text>
+    <Text style={educationStyles.title}>AWS Cloud Practitioner Certificate, February 2023</Text>
+    <Text style={educationStyles.details}>Amazon Web Services Remote, PA</Text>
+    <Text style={educationStyles.title}>Certified Scrum Master, January 2024</Text>
+    <Text style={educationStyles.details}>Scrum Alliance Remote, PA</Text>
+  </View>
+);
+
+const resumeStyles = StyleSheet.create({
+  page: {
+    padding: 30,
+  },
+});
+
+Font.register({
+  family: "Lato",
+  src: "https://fonts.gstatic.com/s/lato/v16/S6uyw4BMUTPHjx4wWw.ttf",
+});
+
+Font.register({
+  family: "Lato Italic",
+  src: "https://fonts.gstatic.com/s/lato/v16/S6u8w4BMUTPHjxsAXC-v.ttf",
+});
+
+Font.register({
+  family: "Lato Bold",
+  src: "https://fonts.gstatic.com/s/lato/v16/S6u9w4BMUTPHh6UVSwiPHA.ttf",
+});
+
 const PDFFile = () => (
-    <Document
-      author="Luke Skywalker"
-      keywords="awesome, resume, start wars"
-      subject="The resume of Luke Skywalker"
-      title="Resume"
-    >
-      <Resume size="A4" />
-      <Resume orientation="landscape" size="A4" />
-      <Resume size={[380, 1250]} />
-    </Document>
-  );
-  
-  export default PDFFile;
+  <Document
+    author="Nan Wroblewski"
+    keywords="resume, software engineer, JavaScript, React, PHP, SpringBoot, SQL, AWS, Agile"
+    subject="Resume"
+    title="Nan Wroblewski Resume"
+  >
+    <Page size="A4" style={resumeStyles.page}>
+      <Header />
+      <Skills />
+      <Experience />
+      <Education />
+    </Page>
+  </Document>
+);
+
+export default PDFFile;
